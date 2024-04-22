3fc9c64e244345db4c46d3005561b1ec32e4d2e7
Boydell Bown <38111+boydell@users.noreply.github.com>
2024-04-22 09:37:21 -0600

    Update findbig.go
    Swap Tabs for spaces

--- findbig.go
+++ findbig.go
@@ -4,51 +4,51 @@ import (
     "fmt"
     "os"
     "path/filepath"
-		"sort"
+    "sort"
 )
 
 type FileInfo struct {
-	size int
-	filename string
+  size int
+  filename string
 }
 
 func main() {
-	mb := int64(1024*1024)
-	list := []FileInfo{}
+  mb := int64(1024*1024)
+  list := []FileInfo{}
 
-	// Get current directory
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+  // Get current directory
+  dir, err := os.Getwd()
+  if err != nil {
+    fmt.Println("Error:", err)
+    return
+  }
 
-	// Walk through all directories and files
-	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Println("Error:", err)
-			return err
-		}
+  // Walk through all directories and files
+  err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+      fmt.Println("Error:", err)
+      return err
+    }
 
-		// Check if it's a regular file and its size is greater than 100MB
-		if info.Mode().IsRegular() && info.Size() > 100*mb {
-			file := FileInfo{int(info.Size() / mb), path}
-			list = append(list, file)
-		}
-		return nil
-	})
+    // Check if it's a regular file and its size is greater than 100MB
+    if info.Mode().IsRegular() && info.Size() > 100*mb {
+      file := FileInfo{int(info.Size() / mb), path}
+      list = append(list, file)
+    }
+    return nil
+  })
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
+  if err != nil {
+    fmt.Println("Error:", err)
+    return
+  }
 
-	// Sort the list by filesize, desc
-	sort.Slice(list, func(i, j int) bool {
-		return list[i].size > list[j].size
-	})
+  // Sort the list by filesize, desc
+  sort.Slice(list, func(i, j int) bool {
+    return list[i].size > list[j].size
+  })
 
-	for _, file := range list {
-		fmt.Printf("[%d MB] %s\n", file.size, file.filename)
-	}
+  for _, file := range list {
+    fmt.Printf("[%d MB] %s\n", file.size, file.filename)
+  }
 }
